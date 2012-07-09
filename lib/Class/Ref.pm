aed63d354a78f07d6256eb12cfa71ca042d3777e
William Cox <wcox@blackducksoftware.com>
2012-07-09 00:29:38 -0400

    really got array's working

--- lib/Class/Ref.pm
+++ lib/Class/Ref.pm
@@ -7,28 +7,37 @@ use Scalar::Util ();
 
 our $VERSION = 'v0.01';
 
+# disable defaults at your peril
+our %nowrap = map { ($_ => 1) } (
+    'Regexp', 'CODE', 'SCALAR', 'REF', 'LVALUE', 'VSTRING',
+    'GLOB',   'IO',   'FORMAT'
+);
+
+my $bless = sub {
+    my ($class, $ref) = @_;
+    my $type = Scalar::Util::reftype $ref;
+    return bless \$ref => "$class\::$type";
+};
+
 my $test = sub {
     return unless $_[0];
     return unless ref $_[0];
     return if Scalar::Util::blessed $_[0];
-    return if 'Regexp' eq Scalar::Util::reftype $_[0];
-    return if 'SCALAR' eq Scalar::Util::reftype $_[0];    # seg faults
+    return if $nowrap{ Scalar::Util::reftype $_[0] };
     1;
 };
 
 my $assign = sub {
     my $v = shift;
     $$v = pop if @_;
-    my $o = $test->($$v) ? \__PACKAGE__->new($$v) : $v;
+    my $o = $test->($$v) ? \__PACKAGE__->$bless($$v) : $v;
     return $o;
 };
 
 sub new {
     my ($class, $ref) = @_;
     die "not a valid reference for $class" unless $test->($ref);
-
-    my $type = Scalar::Util::reftype $ref;
-    return bless \$ref => "$class\::$type";
+    return $class->$bless($ref);
 }
 
 package Class::Ref::HASH;
@@ -37,11 +46,13 @@ use overload '%{}' => sub { ${ $_[0] } };
 
 our $AUTOLOAD;
 
-sub AUTOLOAD : lvalue {
+# TODO research usefulness of lvalue functionality
+sub AUTOLOAD    #: lvalue
+{
     my $self = shift;
     my ($name) = $AUTOLOAD =~ /([^:]+)$/;
     my $o = $assign->(\$$self->{$name}, @_);
-    $$o;
+    $$o;        # FIXME lvalue is lost if $o is a ref wrapper
 }
 
 sub DESTROY { }
@@ -49,21 +60,41 @@ sub DESTROY { }
 package Class::Ref::ARRAY;
 
 use overload '@{}' => sub {
-    tie my @a, __PACKAGE__ . '::Tie';
-    @a = @{ ${ $_[0] } };    # shallow copy
-    return \@a;
+    # tie a proxy array around the real one
+    tie my @a, __PACKAGE__ . '::Tie', ${ $_[0] };
+    \@a;
 };
 
 package Class::Ref::ARRAY::Tie;
 
-use Tie::Array;
-use base 'Tie::StdArray';
+# borrowed from Tie::StdArray (in Tie::Array)
 
-sub FETCH {
-    my ($self, $i) = @_;
-    my $o = $assign->(\$self->[$i]);
-    $$o;
+# ready... steady... go cross-eyed!!
+sub TIEARRAY { bless [$_[1]] => $_[0] }
+sub FETCHSIZE { scalar @{ $_[0][0] } }
+sub STORESIZE { $#{ $_[0][0] } = $_[1] - 1 }
+sub STORE     { $_[0][0]->[$_[1]] = $_[2] }
+sub FETCH     { ${ $assign->(\$_[0][0][$_[1]]) } }      # magic
+sub CLEAR     { @{ $_[0][0] } = () }
+sub POP       { pop @{ $_[0][0] } }
+sub PUSH      { my $o = shift->[0]; push @$o, @_ }
+sub SHIFT     { shift @{ $_[0][0] } }
+sub UNSHIFT   { my $o = shift->[0]; unshift @$o, @_ }
+sub EXISTS    { exists $_[0][0]->[$_[1]] }
+sub DELETE    { delete $_[0][0]->[$_[1]] }
+
+sub SPLICE {
+    my $ob  = shift;
+    my $sz  = $ob->FETCHSIZE;
+    my $off = @_ ? shift : 0;
+    $off += $sz if $off < 0;
+    my $len = @_ ? shift : $sz - $off;
+    splice @{ $ob->[0] }, $off, $len, @_;
 }
+
+##
+## These are bypassed via %nowrap for safety/sanity
+##
 
 package Class::Ref::CODE;
 
